dda25aa0b8a3df1e3b472d5f318718578a43bc8c
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-12-29 14:30:11 +0100

    centre prompt fix marker deletion not being canceled when switching to another tab reformat code remove unused state flag

--- src/main/java/dev/lambdaurora/lambdamap/gui/ConfirmDeletionWidget.java
+++ src/main/java/dev/lambdaurora/lambdamap/gui/ConfirmDeletionWidget.java
@@ -3,15 +3,14 @@ package dev.lambdaurora.lambdamap.gui;
 import dev.lambdaurora.spruceui.Position;
 import dev.lambdaurora.spruceui.background.EmptyBackground;
 import dev.lambdaurora.spruceui.widget.SpruceButtonWidget;
-import dev.lambdaurora.spruceui.widget.SpruceWidget;
 import dev.lambdaurora.spruceui.widget.container.SpruceContainerWidget;
-import dev.lambdaurora.spruceui.widget.container.SpruceParentWidget;
-import net.minecraft.client.render.LightmapTextureManager;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.text.LiteralText;
-import net.minecraft.util.math.Matrix4f;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.Text;
 
 public class ConfirmDeletionWidget extends SpruceContainerWidget {
     private MarkerListWidget.MarkerEntry markerEntry;
@@ -19,26 +18,28 @@ public class ConfirmDeletionWidget extends SpruceContainerWidget {
     public ConfirmDeletionWidget(MarkerTabWidget parent, Position position, int width, int height) {
         super(position, width, height);
         this.setBackground(EmptyBackground.EMPTY_BACKGROUND);
-        int x = 25;
         SpruceButtonWidget cancelButton = new SpruceButtonWidget(Position.of(this, 10, this.getHeight() / 3 * 2 - 10), this.getWidth() / 2 - 20, 20, new LiteralText("Cancel"), button -> {
             parent.switchBack();
         });
         SpruceButtonWidget deleteButton = new SpruceButtonWidget(Position.of(this, getWidth() / 2 + 10, this.getHeight() / 3 * 2 - 10), this.getWidth() / 2 - 20, 20, new LiteralText("I'm sure"), button -> {
-            markerEntry.parent.removeMarker(markerEntry);
+            this.markerEntry.parent.removeMarker(markerEntry);
             parent.switchBack();
         });
-        addChild(cancelButton);
-        addChild(deleteButton);
+        this.addChild(cancelButton);
+        this.addChild(deleteButton);
     }
 
     @Override
     protected void renderWidget(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         this.forEach(child -> child.render(matrices, mouseX, mouseY, delta));
-        int light = LightmapTextureManager.pack(15, 15);
+
         VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
-        Matrix4f model = matrices.peek().getPositionMatrix();
-        String name = markerEntry.marker.getName() == null ? "Unnamed" : markerEntry.marker.getName().asString();
-        this.client.textRenderer.draw("Are you sure you want to delete " + name + "?", this.getX() + 15, height / 3.f, 0xffffffff, true, model, immediate, false, 0, light);
+
+        String name = this.markerEntry.marker.getName() == null ? "Unnamed Marker" : this.markerEntry.marker.getName().asString();
+        OrderedText prompt = Text.of("Are you sure you want to delete " + name + "?").asOrderedText();
+
+        DrawableHelper.drawCenteredTextWithShadow(matrices, this.client.textRenderer, prompt, this.getX() + this.getWidth() / 2, this.getHeight() / 3, 0xffffffff);
+
         immediate.draw();
     }
 
--- src/main/java/dev/lambdaurora/lambdamap/gui/MarkerTabWidget.java
+++ src/main/java/dev/lambdaurora/lambdamap/gui/MarkerTabWidget.java
@@ -3,14 +3,12 @@ package dev.lambdaurora.lambdamap.gui;
 import dev.lambdaurora.lambdamap.LambdaMap;
 import dev.lambdaurora.lambdamap.map.marker.MarkerManager;
 import dev.lambdaurora.spruceui.Position;
-import dev.lambdaurora.spruceui.navigation.NavigationDirection;
 import dev.lambdaurora.spruceui.widget.container.SpruceContainerWidget;
 
 public class MarkerTabWidget extends SpruceContainerWidget {
     private final MarkerListWidget list;
     private final NewMarkerFormWidget newMarkerFormWidget;
     private final ConfirmDeletionWidget confirmDeletionWidget;
-    private boolean promptForDeletion = false;
 
     public MarkerTabWidget(LambdaMap mod, Position position, int width, int height) {
         super(position, width, height);
@@ -18,36 +16,34 @@ public class MarkerTabWidget extends SpruceContainerWidget {
         MarkerManager markers = mod.getMap().getMarkerManager();
 
         int newMarkerFormHeight = width < 480 ? 80 : 40;
-        list = new MarkerListWidget(this, Position.origin(), width, height - newMarkerFormHeight, markers);
-        newMarkerFormWidget = new NewMarkerFormWidget(Position.of(this, 0, list.getHeight()), width, newMarkerFormHeight, markers, list);
-        confirmDeletionWidget = new ConfirmDeletionWidget(this, Position.origin(), width, height);
+        this.list = new MarkerListWidget(this, Position.origin(), width, height - newMarkerFormHeight, markers);
+        this.newMarkerFormWidget = new NewMarkerFormWidget(Position.of(this, 0, list.getHeight()), width, newMarkerFormHeight, markers, list);
+        this.confirmDeletionWidget = new ConfirmDeletionWidget(this, Position.origin(), width, height);
 
-        addChild(list);
-        addChild(newMarkerFormWidget);
-        addChild(confirmDeletionWidget);
-        switchBack();
+        this.addChild(list);
+        this.addChild(newMarkerFormWidget);
+        this.addChild(confirmDeletionWidget);
+        this.switchBack();
     }
 
     public void promptForDeletion(MarkerListWidget.MarkerEntry entry) {
-        this.promptForDeletion = true;
-        list.setVisible(false);
-        newMarkerFormWidget.setVisible(false);
-        confirmDeletionWidget.setVisible(true);
-        confirmDeletionWidget.setMarkerEntry(entry);
+        this.list.setVisible(false);
+        this.newMarkerFormWidget.setVisible(false);
+        this.confirmDeletionWidget.setVisible(true);
+        this.confirmDeletionWidget.setMarkerEntry(entry);
     }
 
     public void switchBack() {
-        this.promptForDeletion = false;
-        list.setVisible(true);
-        newMarkerFormWidget.setVisible(true);
-        confirmDeletionWidget.setVisible(false);
+        this.list.setVisible(true);
+        this.newMarkerFormWidget.setVisible(true);
+        this.confirmDeletionWidget.setVisible(false);
     }
 
     @Override
-    public boolean onNavigation(NavigationDirection direction, boolean tab) {
-        switchBack();
-        return super.onNavigation(direction, tab);
+    public void setFocused(boolean focused) {
+        if(!focused) {
+            this.switchBack();
+        }
+        super.setFocused(focused);
     }
-
-
 }
