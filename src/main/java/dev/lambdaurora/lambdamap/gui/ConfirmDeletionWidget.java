100e511e3da4ae4eaa8944608c30820dd215e0d1
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2021-12-29 14:54:53 +0100

    move literal texts to translations

--- src/main/java/dev/lambdaurora/lambdamap/gui/ConfirmDeletionWidget.java
+++ src/main/java/dev/lambdaurora/lambdamap/gui/ConfirmDeletionWidget.java
@@ -8,9 +8,8 @@ import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.util.math.MatrixStack;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.OrderedText;
-import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 public class ConfirmDeletionWidget extends SpruceContainerWidget {
     private MarkerListWidget.MarkerEntry markerEntry;
@@ -18,10 +17,8 @@ public class ConfirmDeletionWidget extends SpruceContainerWidget {
     public ConfirmDeletionWidget(MarkerTabWidget parent, Position position, int width, int height) {
         super(position, width, height);
         this.setBackground(EmptyBackground.EMPTY_BACKGROUND);
-        SpruceButtonWidget cancelButton = new SpruceButtonWidget(Position.of(this, 10, this.getHeight() / 3 * 2 - 10), this.getWidth() / 2 - 20, 20, new LiteralText("Cancel"), button -> {
-            parent.switchBack();
-        });
-        SpruceButtonWidget deleteButton = new SpruceButtonWidget(Position.of(this, getWidth() / 2 + 10, this.getHeight() / 3 * 2 - 10), this.getWidth() / 2 - 20, 20, new LiteralText("I'm sure"), button -> {
+        SpruceButtonWidget cancelButton = new SpruceButtonWidget(Position.of(this, 10, this.getHeight() / 3 * 2 - 10), this.getWidth() / 2 - 20, 20, new TranslatableText("lambdamap.marker.confirm_deletion.cancel"), button -> parent.switchBack());
+        SpruceButtonWidget deleteButton = new SpruceButtonWidget(Position.of(this, getWidth() / 2 + 10, this.getHeight() / 3 * 2 - 10), this.getWidth() / 2 - 20, 20, new TranslatableText("lambdamap.marker.confirm_deletion.confirm"), button -> {
             this.markerEntry.parent.removeMarker(markerEntry);
             parent.switchBack();
         });
@@ -35,8 +32,10 @@ public class ConfirmDeletionWidget extends SpruceContainerWidget {
 
         VertexConsumerProvider.Immediate immediate = VertexConsumerProvider.immediate(Tessellator.getInstance().getBuffer());
 
-        String name = this.markerEntry.marker.getName() == null ? "Unnamed Marker" : this.markerEntry.marker.getName().asString();
-        OrderedText prompt = Text.of("Are you sure you want to delete " + name + "?").asOrderedText();
+        String name = this.markerEntry.marker.getName() == null ? "" : this.markerEntry.marker.getName().asString();
+        OrderedText prompt = new TranslatableText("lambdamap.marker.confirm_deletion.prompt",
+                    name.equals("") ? new TranslatableText("lambdamap.marker.confirm_deletion.prompt.unnamed") : name)
+                .asOrderedText();
 
         DrawableHelper.drawCenteredTextWithShadow(matrices, this.client.textRenderer, prompt, this.getX() + this.getWidth() / 2, this.getHeight() / 3, 0xffffffff);
 
