00ff65a61e6131d5dec8e641e605899ba20a4f0c
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2022-01-25 15:05:40 +0100

    Revert "Fix marker deletion prompt being dismissable without interaction"
    This reverts commit 2904064178c1940a8c80a3f5bd732fd7da9e1093.

--- src/main/java/dev/lambdaurora/lambdamap/gui/MarkerTabWidget.java
+++ src/main/java/dev/lambdaurora/lambdamap/gui/MarkerTabWidget.java
@@ -61,4 +61,12 @@ public class MarkerTabWidget extends SpruceContainerWidget {
 			this.setFocused(true);
 		return result;
 	}
+
+	@Override
+	public void setFocused(boolean focused) {
+		if(!focused) {
+			this.switchBack();
+		}
+		super.setFocused(focused);
+	}
 }
