5762556b1f6c2b17c64c49e9c203dd5c083891eb
tibs <git@tibinonest.me>
2023-03-23 18:26:26 -0400

    Some fixes
    Yes it's cursed, but pain

--- src/main/java/dev/lambdaurora/lambdamap/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/dev/lambdaurora/lambdamap/mixin/ClientPlayNetworkHandlerMixin.java
@@ -18,29 +18,17 @@
 package dev.lambdaurora.lambdamap.mixin;
 
 import dev.lambdaurora.lambdamap.LambdaMap;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.client.world.ClientWorld;
 import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket;
 import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
 import net.minecraft.network.packet.s2c.play.ChunkDeltaUpdateS2CPacket;
-import net.minecraft.network.packet.s2c.play.GameJoinS2CPacket;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPlayNetworkHandler.class)
 public class ClientPlayNetworkHandlerMixin {
-	@Final
-	@Shadow
-	private MinecraftClient client;
-
-	@Shadow
-	private ClientWorld world;
-
 	@Inject(method = "onChunkData", at = @At("RETURN"))
 	private void onChunkData(ChunkDataS2CPacket packet, CallbackInfo ci) {
 		LambdaMap.get().onChunkUpdate(packet.chunkX(), packet.chunkZ());
@@ -55,14 +43,5 @@ public class ClientPlayNetworkHandlerMixin {
 	@Inject(method = "onBlockUpdate", at = @At("RETURN"))
 	private void onBlockUpdate(BlockUpdateS2CPacket packet, CallbackInfo ci) {
 		LambdaMap.get().onBlockUpdate(packet.getPos().getX(), packet.getPos().getZ());
-	}
-
-	@Inject(method = "onGameJoin", at = @At("TAIL"))
-	private void onGameJoin(GameJoinS2CPacket packet, CallbackInfo ci) {
-		LambdaMap.get().unloadMap();
-
-		if (world != null) {
-			LambdaMap.get().loadMap(client, world);
-		}
 	}
 }
---
+++ src/main/java/dev/lambdaurora/lambdamap/mixin/ClientPlayerEntityMixin.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2021-2022 LambdAurora <email@lambdaurora.dev>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package dev.lambdaurora.lambdamap.mixin;
+
+import dev.lambdaurora.lambdamap.LambdaMap;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public class ClientPlayerEntityMixin {
+    @Final
+    @Shadow
+    protected MinecraftClient client;
+
+    @Inject(method = "init", at = @At("HEAD"))
+    private void onInit(CallbackInfo ci) {
+        LambdaMap.get().unloadMap();
+        LambdaMap.get().loadMap(client, client.world);
+    }
+}
